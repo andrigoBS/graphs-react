6d50c0b8baf29f3097c5474ecd99843ccd7ac20c
karol <karolinesouzaguckert@gmail.com>
2020-09-21 01:32:07 -0300

    alteração página

--- src/component/leftbar/LeftBar.jsx
+++ src/component/leftbar/LeftBar.jsx
@@ -7,6 +7,7 @@ import ListItem from "@material-ui/core/ListItem";
 import Collapse from "@material-ui/core/Collapse";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 
 const drawerWidth = 200;
@@ -18,8 +19,47 @@ const useStyles = makeStyles(theme =>({
 
     space:{
         marginTop: '12px'
-    }
+    },
 
+    buttonStyle:{
+        marginTop: '12px',
+        color:'#9c27b0',
+        marginBottom: '12px'
+    },
+
+    distance:{
+        marginTop:'12px',
+        marginLeft:'10px',
+        marginRight:'10px',
+        color:'#9c27b0',
+    },
+
+    margins:{
+        marginTop:'-10px',
+        marginBottom:'-10px'
+    },
+
+    marginTopClose:{
+      marginTop:'-15px'
+    },
+
+    marginTopOpen:{
+        marginTop:'15px'
+    },
+
+
+    // colorOpen:{
+    //     backgroundColor:'#fdefff',
+    // },
+
+    colorClose:{
+        backgroundColor:'#ffffff'
+    },
+
+    optionsFont:{
+      color:'#7e7e7e',
+        fontSize: 15
+    }
 
 }));
 const LeftBar = () =>{
@@ -30,6 +70,7 @@ const LeftBar = () =>{
     const handleOnClickAddVertex = () => {
         setAddVertex(!addVertex);
     };
+
     return(
         <div >
             <Drawer
@@ -39,39 +80,51 @@ const LeftBar = () =>{
             }}>
                 <Toolbar/>
                 <div >
-                    <List >
-                        <ListItem button onClick={handleOnClickAddVertex}>
-                          ADICIONAR VERTICE
-                        </ListItem>
-                        <ListItem>
-                            <Collapse in={addVertex}>
-                                <TextField
-                                    id="vertextName"
-                                    label="Nome"
-                                    type="text"
-                                    variant="outlined"
-                                    size={"small"}
-                                />
-                                <TextField
-                                    id="vertextName"
-                                    label="Peso"
-                                    type="text"
-                                    variant="outlined"
-                                    size={"small"}
-                                    className={styles.space}
-                                />
-                                <div align={"center"}>
-                                    <Button size={"small"} className={styles.space} >
-                                        Adicionar
-                                    </Button>
-                                </div>
+                    <List>
 
+                        <Divider/>
+                        <ListItem className={addVertex ? styles.colorOpen :styles.colorClose} button onClick={handleOnClickAddVertex}>
+                            <div className={styles.margins}>
+                                <h5 className={styles.optionsFont}>ADICIONAR VERTICE</h5>
+                            </div>
+                        </ListItem>
+
+                        <ListItem >
+                            <Collapse  in={addVertex}>
+                                <Paper variant={"outlined"}>
+                                        <TextField
+                                            id="vertextName"
+                                            label="Nome"
+                                            type="text"
+                                            variant="outlined"
+                                            size={"small"}
+                                            className={styles.distance}
+                                            color={"primary"}
+                                        />
+
+
+                                    <div align={"center"}>
+                                        <Button
+                                            color={"primary"}
+                                            onClick={handleOnClickAddVertex}
+                                            size={"small"}
+                                            className={styles.buttonStyle} >
+                                            Adicionar
+                                        </Button>
+                                    </div>
+                                </Paper>
                             </Collapse>
                         </ListItem>
 
+                        <Divider className={addVertex ? styles.marginTopOpen : styles.marginTopClose}/>
                         <ListItem button>
-                            ADICIONAR ARESTA
+                            <div className={styles.margins} align={"center"}>
+                                <h5 className={styles.optionsFont}>ADICIONAR ARESTA</h5>
+                            </div>
+
                         </ListItem>
+
+
                         <ListItem button>
                             ADICIONAR ARCO
                         </ListItem>
